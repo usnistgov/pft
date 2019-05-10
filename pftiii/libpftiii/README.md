6c4b07c9ab26ed86a79eb8972b99adf7376c1546
Gregory <gregory.fiumara@nist.gov>
2019-05-10 11:57:11 -0400

    Add libpftiii.

---
+++ pftiii/libpftiii/README.md
@@ -0,0 +1,46 @@
+`libpftiii`
+===============
+
+`libpftiii` is a shared library containing implementations for methods defined
+in [`pftiii.h`]. *Core* PFT III libraries must link against `libpftiii`.
+
+NIST will build a clean copy of `libpftiii` and link it against the main
+evaluation executable along with the PFT III core library. This is to ensure
+that the implementations of `libpftiii` are equivalent for all participants, and
+is additionally the reason why these methods were not implemented directly in
+[`pftiii.h`].
+
+Building
+--------
+Execute `make` in this directory and `libpftiii.so` will be created. It will
+additionally be copied into the [validation] `lib` directory.
+
+Linking
+-------
+When building a core PFT III library, use these example compiler flags (from
+`g++`) to properly link against this library.
+
+> `-L/path/to/libpftiii -lpftiii -Wl,-rpath,/path/to/libpftiii`
+
+Communication
+-------------
+If you found a bug and can provide steps to reliably reproduce it, or if you
+have a feature request, please [open an issue]. Other questions may be addressed
+to the [NIST PFT team].
+
+The PFT team sends updates about the PFT tests to their mailing list. Enter your
+e-mail address on the [mailing list site], or send a blank e-mail to
+PFT+subscribe@list.nist.gov to be automatically subscribed. Posts to the list
+are mirrored on an [RSS feed].
+
+License
+-------
+The items in this repository are released in the public domain. See the
+[LICENSE] for details.
+
+[`pftiii.h`]: https://github.com/usnistgov/pft/blob/master/pftiii/include/pftiii.h
+[NIST PFT team]: mailto:pft@nist.gov
+[open an issue]: https://github.com/usnistgov/pft/issues
+[mailing list site]: https://groups.google.com/a/list.nist.gov/forum/#!forum/pft/join
+[RSS feed]: https://groups.google.com/a/list.nist.gov/forum/feed/pft/msgs/rss.xml
+[LICENSE]: https://github.com/usnistgov/pft/blob/master/LICENSE.md
---
+++ pftiii/libpftiii/libpftiii.cpp
@@ -0,0 +1,146 @@
+/*
+ * This software was developed at the National Institute of Standards and
+ * Technology (NIST) by employees of the Federal Government in the course
+ * of their official duties. Pursuant to title 17 Section 105 of the
+ * United States Code, this software is not subject to copyright protection
+ * and is in the public domain. NIST assumes no responsibility whatsoever for
+ * its use by other parties, and makes no guarantees, expressed or implied,
+ * about its quality, reliability, or any other characteristic.
+ */
+
+#include <pftiii.h>
+
+PFTIII::FingerImage::FingerImage()
+{
+
+}
+
+PFTIII::FingerImage::FingerImage(
+    const uint16_t width,
+    const uint16_t height,
+    const uint16_t ppi,
+    const std::vector<uint8_t> &pixels,
+    const Impression imp,
+    const FrictionRidgeCaptureTechnology frct,
+    const FrictionRidgeGeneralizedPosition frgp) :
+    width{width},
+    height{height},
+    ppi{ppi},
+    pixels{pixels},
+    imp{imp},
+    frct{frct},
+    frgp{frgp}
+{
+
+}
+
+/******************************************************************************/
+
+PFTIII::CreateProprietaryTemplateResult
+PFTIII::CreateProprietaryTemplateResult::failure(
+    const std::string &message)
+{
+	CreateProprietaryTemplateResult cptr{};
+
+	cptr.result = Result::Failure;
+	cptr.proprietaryTemplate = {};
+	cptr.message = message;
+
+	return (cptr);
+}
+
+PFTIII::CreateProprietaryTemplateResult
+PFTIII::CreateProprietaryTemplateResult::success(
+    const std::vector<uint8_t> &proprietaryTemplate,
+    const std::string &message)
+{
+	CreateProprietaryTemplateResult cptr{};
+
+	cptr.result = Result::Success;
+	cptr.proprietaryTemplate = proprietaryTemplate;
+	cptr.message = message;
+
+	return (cptr);
+}
+
+/******************************************************************************/
+
+PFTIII::SubmissionIdentification::SubmissionIdentification()
+{
+
+}
+
+PFTIII::SubmissionIdentification::SubmissionIdentification(
+    const uint16_t versionNumber,
+    const std::string &libraryIdentifier,
+    const std::tuple<std::string, bool>
+        &featureExtractionAlgorithmMarketingIdentifier,
+    const std::tuple<std::string, bool> &comparisonAlgorithmMarketingIdentifier,
+    const std::tuple<uint16_t, bool> &cbeffProductOwner,
+    const std::tuple<uint16_t, bool> &cbeffFeatureExtractionAlgorithmIdentifier,
+    const std::tuple<uint16_t, bool> &cbeffComparisonAlgorithmIdentifier):
+    versionNumber{versionNumber},
+    libraryIdentifier{libraryIdentifier},
+    featureExtractionAlgorithmMarketingIdentifier{
+        featureExtractionAlgorithmMarketingIdentifier},
+    comparisonAlgorithmMarketingIdentifier{
+        comparisonAlgorithmMarketingIdentifier},
+    cbeffProductOwner{cbeffProductOwner},
+    cbeffFeatureExtractionAlgorithmIdentifier{
+        cbeffFeatureExtractionAlgorithmIdentifier},
+    cbeffComparisonAlgorithmIdentifier{cbeffComparisonAlgorithmIdentifier}
+{
+
+}
+
+/******************************************************************************/
+
+PFTIII::FingerImageStatus::FingerImageStatus(
+    const Code code,
+    const std::string &message) :
+    code{code},
+    message{message}
+{
+
+}
+
+/******************************************************************************/
+
+PFTIII::CompareProprietaryTemplatesStatus::CompareProprietaryTemplatesStatus(
+    const Result result,
+    const std::string &message) :
+    result{result},
+    message{message}
+{
+
+}
+
+PFTIII::CompareProprietaryTemplatesStatus
+PFTIII::CompareProprietaryTemplatesStatus::failure(
+    const std::string &message)
+{
+	CompareProprietaryTemplatesStatus cpts{};
+
+	cpts.result = Result::Failure;
+	cpts.message = message;
+
+	return (cpts);
+}
+
+PFTIII::CompareProprietaryTemplatesStatus
+PFTIII::CompareProprietaryTemplatesStatus::success()
+{
+	CompareProprietaryTemplatesStatus cpts{};
+
+	cpts.result = Result::Success;
+	cpts.message = "";
+
+	return (cpts);
+}
+
+/******************************************************************************/
+
+PFTIII::Interface::~Interface()
+{
+
+}
